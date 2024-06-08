a00879e3312f9b7534c6581895cf548bd32de1e3
theinit01 <tushar3145@gmail.com>
2024-06-08 23:24:55 +0530

    initial commits

--- save_model.py
+++ save_model.py
@@ -8,6 +8,7 @@ import nltk
 from nltk.corpus import stopwords 
 import string
 from sklearn.metrics import accuracy_score, classification_report
+import joblib
 
 # Download stopwords if not already downloaded
 nltk.download('stopwords')
@@ -17,11 +18,11 @@ stopwords = set(stopwords.words("english"))
 
 df = pd.read_csv(r"C:\Users\tusha\OneDrive\Desktop\Twitter-Hate-Speech-Classifier\model\twitter_data.csv")
 
-# Map class labels correctly
 df['labels'] = df['class'].map({0: "Hate Speech Detected", 1: "Offensive language detected", 2: "No hate and Offensive speech"})
 
 df = df[['tweet', 'labels']]
 df.dropna(inplace=True)
+
 
 def clean(text):
     text = str(text).lower()
@@ -33,38 +34,30 @@ def clean(text):
     text = re.sub('\w*\d\w*', '', text)
     text = ' '.join(word for word in text.split(' ') if word not in stopwords)
     return text
-def predict_sentence(sentence, vectorizer, classifier):
-    # Clean the sentence
-    cleaned_sentence = clean(sentence)
-    # Transform the cleaned sentence into a numerical representation
-    vectorized_sentence = vectorizer.transform([cleaned_sentence])
-    # Predict the label for the vectorized sentence
-    predicted_label = classifier.predict(vectorized_sentence)
-    return predicted_label[0]
-
-
 
 
 df["tweet"] = df["tweet"].apply(clean)
 
+# Features and labels
 x = np.array(df["tweet"])
 y = np.array(df["labels"])
 
+# Vectorize text
 cv = CountVectorizer()
 x = cv.fit_transform(x)
 
+
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=42)
 
+# Train model
 clf = DecisionTreeClassifier()
 clf.fit(X_train, y_train)
 
-# Example sentence to predict
-new_sentence = "I hate your country"
+# Save the model and CountVectorizer
+# joblib.dump(clf, 'hate_speech_classifier.pkl')
+# joblib.dump(cv, 'count_vectorizer.pkl')
 
-# Predict the label for the new sentence
-predicted_label = predict_sentence(new_sentence, cv, clf)
-print("Predicted Label:", predicted_label)
-
+# Evaluate model
 accuracy = accuracy_score(y_test, clf.predict(X_test))
 report = classification_report(y_test, clf.predict(X_test))
 
