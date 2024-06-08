02cee2b9aad42fb84bef86bbfa73654457d24edf
theinit01 <tushar3145@gmail.com>
2024-06-09 00:53:01 +0530

    Fianl commits

--- model/webApp.py
+++ model/webApp.py
@@ -49,6 +49,7 @@ if username != '' and '@' not in username:
         results[predicted_label].append(tweet)
         all_tweets_text += " " + tweet
 
+    '''
     # Count the number of tweets in each category
     categories = list(results.keys())
     counts = [len(results[category]) for category in categories]
@@ -81,3 +82,7 @@ if username != '' and '@' not in username:
         st.pyplot(piefig)
     with col3:
         st.pyplot(wordcloud_fig)
+
+        '''
+    
+    st.write(results)
